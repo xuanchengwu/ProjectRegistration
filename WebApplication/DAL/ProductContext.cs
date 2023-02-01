51ca20684c71ce0915980636dd6ee8fc4cf8327d
xuanchengwu <wxc1121@gmail.com>
2023-02-01 05:18:35 -0500

    Display Products

---
+++ WebApplication/DAL/ProductContext.cs
@@ -0,0 +1,21 @@
+﻿using WebApplication.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace WebApplication.DAL
+{
+    public class ProductContext : DbContext
+    {
+
+        public ProductContext() : base("ProductContext")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+      
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ WebApplication/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication.Models
+{
+    [Table("POC_Marketplace")]
+    public class Product
+    {
+        [Key]
+        public int Product_ID { get; set; }
+        public string Product_Name { get; set; }
+        public string Category { get; set; }
+        public int subCategoryID { get; set; }
+        public string Seris { get; set; }
+        public string Seris_Info { get; set; }
+        public string Feature { get; set; }
+        public string Model { get; set; }
+        public int Model_Year { get; set; }
+
+
+    
+    }
+}
